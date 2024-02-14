33e5745b5fea73d04f1dc1bbf8fcd27b8b357e66
Koffeekup2610 <127402902+Koffeekup2610@users.noreply.github.com>
2024-02-14 16:24:20 +0000

    Delete Q5.sql

--- Q5.sql
+++
@@ -1,7 +0,0 @@
-SELECT product_category, SUM(profit) AS total_profit
-FROM sales
-WHERE EXTRACT(YEAR FROM date) = 2021
-  AND region = 'Wiltshire, UK'
-GROUP BY product_category
-ORDER BY total_profit DESC
-LIMIT 1;
